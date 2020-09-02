24ecd03f526d0c4de7427aaa15362ce9b05ad66d
Thom9521 <thom9521@edu.easj.dk>
2020-09-02 13:24:35 +0200

    Exercises

---
+++ week36/lab/lettercase.php
@@ -0,0 +1,13 @@
+<?php
+
+//Converts all letters to uppercase
+echo strtoupper('hello world!');
+
+//Converts all letters to lowercase
+echo strtolower('HELLO WORLD!');
+
+//Converts the first letter to uppercase
+echo ucfirst("hello world!");
+
+//Converts all the first letter of all the words to uppercase
+echo ucwords("hello world!");
